f0b426c6de870575352423fff1e1e007e1710589
Keita Yamamoto <yamamoto@altonotes.co.jp>
2018-11-08 17:27:31 +0900

    KibaanSampleを追加

---
+++ KibaanSample/KibaanSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KibaanSample
+//
+//  Created by Keita Yamamoto on 2018/11/08.
+//  Copyright © 2018 altonotes Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
