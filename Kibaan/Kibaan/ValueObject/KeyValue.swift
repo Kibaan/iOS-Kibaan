516f8025f12216df58218308dc1f9f0d99653ba0
Keita Yamamoto <yamamoto@altonotes.co.jp>
2018-11-27 16:49:31 +0900

    [ver 0.6.07] KeyValueのコンストラクターをpublicにする

--- Kibaan/Kibaan/ValueObject/KeyValue.swift
+++ Kibaan/Kibaan/ValueObject/KeyValue.swift
@@ -7,4 +7,9 @@ import Foundation
 public struct KeyValue {
     public var key: String
     public var value: String?
+    
+    public init(key: String, value: String?) {
+        self.key = key
+        self.value = value
+    }
 }
