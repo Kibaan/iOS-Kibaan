992ecee88ddbec0c7027d7590a708db3d17ba059
Akira Nakajima <nakajima@altonotes.co.jp>
2019-07-03 12:55:13 +0900

    addNextScreen/removeNextScreenを改修（Maskを使用したクリップ処理ありバージョン）

--- Kibaan/Kibaan/Framework/SmartViewController.swift
+++ Kibaan/Kibaan/Framework/SmartViewController.swift
@@ -23,12 +23,14 @@ open class SmartViewController: UIViewController {
     open var hasOverlay: Bool { return !overlays.isEmpty }
     /// スライド表示させた画面リスト
     private var nextScreens = [SmartViewController]()
-    /// スライド表示させた画面の制約
-    private var nextScreenConstraints: [NSLayoutConstraint] = []
     /// スライド表示させる画面を追加する対象のビュー
-    open var nextScreenTargetView: UIView { return view }
+    open var nextScreenTarget: UIView { return view }
     /// スライドアニメーション時間
     open var nextScreenAnimationDuration: TimeInterval = 0.3
+    /// ビューが隠れるときのスライド幅
+    open var slideWidthOnHide: CGFloat { return view.frame.width / 4 }
+    /// 前画面に戻る為のEdgePanGesture
+    open var edgePanGesture: UIScreenEdgePanGestureRecognizer?
     /// オーバーレイ画面のオーナー
     open weak var owner: SmartViewController?
     /// スライド表示させた画面の遷移のルート
@@ -112,61 +114,34 @@ open class SmartViewController: UIViewController {
         subControllers.append(contentsOf: controllers)
     }
     
-    override open func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        // 画面回転時に制約に設定した"constant"を更新する必要がある
-        if !nextScreens.isEmpty {
-            let constant = nextScreenTargetView.frame.width
-            adjustFirstViewConstraintConstant(parentView: nextScreenTargetView.superview, targetView: nextScreenTargetView, constant: constant)
-        }
-    }
-    
     // MARK: - Next screen
-    
-    /// "targetView"がrootViewに含まれているかをチェックする
-    private func checkTargetView(_ targetView: UIView) {
-        assert(targetView.isDescendant(of: view), "The target view must be included in the view")
-    }
     
     /// ViewControllerをスライド表示させる
     @discardableResult
     open func addNextScreen<T: SmartViewController>(_ type: T.Type, id: String? = nil, cache: Bool = true, animated: Bool = true, prepare: ((T) -> Void)? = nil) -> T? {
-        let targetView = nextScreenTargetView
-        checkTargetView(targetView)
         let controller = ViewControllerCache.shared.get(type, id: id, cache: cache)
-        guard let parentView = targetView.superview, nextScreens.last != controller else {
-            return nil
-        }
         controller.navigationRootController = self
-        let isFirstScreen = nextScreens.isEmpty
         
-        // View追加およびビューのサイズ調整用の制約を適用
-        let prevView = nextScreens.last?.view ?? targetView
-        parentView.addSubview(controller.view)
+        let parent = nextScreenTarget
+        parent.addSubview(controller.view)
         controller.view.translatesAutoresizingMaskIntoConstraints = false
-        controller.view.topAnchor.constraint(equalTo: targetView.topAnchor).isActive = true
-        controller.view.bottomAnchor.constraint(equalTo: targetView.bottomAnchor).isActive = true
-        controller.view.widthAnchor.constraint(equalTo: targetView.widthAnchor).isActive = true
-        controller.view.leadingAnchor.constraint(equalTo: prevView.trailingAnchor).activate(priority: .defaultHigh)
-        parentView.layoutIfNeeded()
+        controller.view.topAnchor.constraint(equalTo: parent.topAnchor).isActive = true
+        controller.view.leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
+        controller.view.bottomAnchor.constraint(equalTo: parent.bottomAnchor).isActive = true
+        controller.view.trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
         
         leave()
         nextScreens += [controller]
         
-        // ビューの表示位置を定める制約を適用
-        let viewWidth = parentView.frame.size.width
-        nextScreenConstraints.last?.priority = .lowest
-        if isFirstScreen {
-            adjustFirstViewConstraintConstant(parentView: parentView, targetView: prevView, constant: viewWidth)
-        }
-        let constraint = prevView.trailingAnchor.constraint(equalTo: parentView.leadingAnchor).activate(priority: .highest)
-        nextScreenConstraints.append(constraint)
-
         if animated {
-            UIView.animate(withDuration: nextScreenAnimationDuration, animations: {
-                parentView.layoutIfNeeded()
-            })
+            prepareForward(nextView: controller.view)
+            forwardAnimation(parent: parent, child: controller.view, duration: nextScreenAnimationDuration)
+        }
+        if edgePanGesture == nil {
+            let gesture = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(self.edgePanAction(gesture:)))
+            gesture.edges = .left
+            view.addGestureRecognizer(gesture)
+            edgePanGesture = gesture
         }
         prepare?(controller)
         controller.added()
@@ -174,73 +149,214 @@ open class SmartViewController: UIViewController {
         return controller
     }
     
-    /// 指定されたビューのX軸の表示位置を"constant"を利用して調整する
-    private func adjustFirstViewConstraintConstant(parentView: UIView?, targetView: UIView?, constant: CGFloat) {
-        // FIXME parentViewのconstraintsを操作しているので、SafeAreaと紐付けた場合に移動できない
-        let targetAttribute: [NSLayoutConstraint.Attribute] = [.leading, .left, .trailing, .right, .centerX]
-        parentView?.constraints.filter {
-            return ($0.firstItem as? UIView) == targetView
-                && ($0.secondItem as? UIView) == parentView
-                && targetAttribute.contains($0.firstAttribute)
-        }.forEach {
-            $0.constant = -constant
-        }
-        parentView?.constraints.filter {
-            return ($0.firstItem as? UIView) == parentView
-                && ($0.secondItem as? UIView) == targetView
-                && targetAttribute.contains($0.secondAttribute)
-        }.forEach {
-            $0.constant = constant
-        }
-    }
-    
     /// スライド表示させたViewControllerを１つ前に戻す
     open func removeNextScreen(animated: Bool = true) {
-        let targetView = nextScreenTargetView
-        targetView.superview?.layoutIfNeeded()
-
-        let removedScreen = self.nextScreens.removeLast()
-        removedScreen.leave()
+        let lastScreen = self.nextScreens.removeLast()
+        lastScreen.leave()
         
         enter()
         
         let completion = {
-            removedScreen.view.removeFromSuperview()
-            removedScreen.removed()
+            lastScreen.view.removeFromSuperview()
+            lastScreen.removed()
+            self.clearEdgePanGesture()
         }
-        nextScreenConstraints.removeLast().isActive = false
-        nextScreenConstraints.last?.priority = .highest
-        if nextScreens.isEmpty {
-            adjustFirstViewConstraintConstant(parentView: targetView.superview, targetView: targetView, constant: 0)
-        }
-        
         if animated {
-            UIView.animate(withDuration: nextScreenAnimationDuration, delay: 0, options: .curveEaseIn, animations: {
-                targetView.superview?.layoutIfNeeded()
-            }, completion: { flag in
-                completion()
-            })
+            prepareBack(nextView: lastScreen.view)
+            backAnimation(parent: nextScreenTarget, child: lastScreen.view, duration: nextScreenAnimationDuration, completion: completion)
         } else {
             completion()
         }
     }
+    
+    /// スライド表示させたViewControllerを１つ前に戻す（EdgePanGestureからのみ呼ばれる）
+    private func removeNextScreenByGesture(parent: UIView, child: UIView, duration: TimeInterval) {
+        let lastScreen = self.nextScreens.removeLast()
+        lastScreen.leave()
+        
+        enter()
+        
+        backAnimation(parent: parent, child: child, duration: duration, completion: {
+            lastScreen.view.removeFromSuperview()
+            lastScreen.removed()
+            self.clearEdgePanGesture()
+        })
+    }
 
     /// スライド表示させたViewControllerを全て閉じる
     open func removeAllNextScreen(executeStart: Bool = false) {
-        guard isViewLoaded else { return }
-        let targetView = nextScreenTargetView
-        leave()
-        nextScreens.forEach {
-            $0.view.removeFromSuperview()
-            $0.removed()
+//        guard isViewLoaded else { return }
+//        let targetView = nextScreenTargetView
+//        leave()
+//        nextScreens.forEach {
+//            $0.view.removeFromSuperview()
+//            $0.removed()
+//        }
+//        nextScreens.removeAll()
+//        nextScreenConstraints.forEach { $0.isActive = false }
+//        nextScreenConstraints.removeAll()
+//        adjustFirstViewConstraintConstant(parentView: targetView.superview, targetView: targetView, constant: 0)
+//        if executeStart {
+//            enter()
+//        }
+    }
+    
+    private func clearEdgePanGesture() {
+        if let gesture = edgePanGesture, nextScreens.isEmpty {
+            nextScreenTarget.removeGestureRecognizer(gesture)
+            edgePanGesture = nil
         }
-        nextScreens.removeAll()
-        nextScreenConstraints.forEach { $0.isActive = false }
-        nextScreenConstraints.removeAll()
-        adjustFirstViewConstraintConstant(parentView: targetView.superview, targetView: targetView, constant: 0)
-        if executeStart {
-            enter()
+    }
+    
+    private func prepareBack(nextView: UIView) {
+        // 影を表示する
+        nextView.layer.shadowOpacity = 1.0
+        // ビューの初期位置を調整
+        nextScreenTarget.transform = CGAffineTransform(translationX: -slideWidthOnHide, y: 0)
+        nextView.transform = CGAffineTransform(translationX: slideWidthOnHide, y: 0)
+        // クリップ用のマスクを設定する
+        setMaskLayerRect(nextScreenTarget.bounds.with(x: slideWidthOnHide), view: nextScreenTarget)
+    }
+    
+    private func prepareForward(nextView: UIView) {
+        // 影をつける
+        setShadow(targetView: nextView, percentage: 0.0)
+        // ビューの初期位置を調整
+        nextView.transform = CGAffineTransform(translationX: nextScreenTarget.frame.width, y: 0)
+        // クリップ用のマスクを設定する
+        setMaskLayerRect(nextScreenTarget.bounds, view: nextScreenTarget)
+    }
+    
+    private func forwardAnimation(parent: UIView, child: UIView, duration: TimeInterval) {
+        parent.isUserInteractionEnabled = false
+        child.isUserInteractionEnabled = false
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: [.curveEaseOut], animations: {
+            parent.transform = CGAffineTransform(translationX: -self.slideWidthOnHide, y: 0)
+            child.transform = CGAffineTransform(translationX: self.slideWidthOnHide, y: 0)
+        }, completion: { _ in
+            parent.transform = .identity
+            parent.isUserInteractionEnabled = true
+            child.transform = .identity
+            child.isUserInteractionEnabled = true
+        })
+        setMaskAnimation(view: parent, x: self.slideWidthOnHide, duration: duration, completion: {
+            parent.layer.mask = nil
+        })
+        setShadowOpacityAnimation(view: child, toValue: 1.0, duration: duration, completion: {
+            child.layer.shadowOpacity = 0.0
+        })
+    }
+    
+    private func backAnimation(parent: UIView, child: UIView, duration: TimeInterval, completion: (() -> Void)? = nil) {
+        parent.isUserInteractionEnabled = false
+        child.isUserInteractionEnabled = false
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: [.curveEaseOut], animations: {
+            parent.transform = .identity
+            child.transform = CGAffineTransform(translationX: child.frame.width, y: 0)
+        }, completion: { result in
+            child.transform = .identity
+            child.isUserInteractionEnabled = true
+            parent.isUserInteractionEnabled = true
+            completion?()
+        })
+        setMaskAnimation(view: parent, x: 0, duration: duration, completion: {
+            parent.layer.mask = nil
+        })
+        setShadowOpacityAnimation(view: child, toValue: 0.0, duration: duration, completion: {
+            child.layer.shadowOpacity = 0.0
+        })
+    }
+    
+    private func setShadow(targetView: UIView, percentage: Float = 1.0) {
+        targetView.layer.shadowOffset = CGSize(width: 6.0, height: 0.0)
+        targetView.layer.shadowColor = UIColor.black.cgColor
+        targetView.layer.shadowOpacity = percentage
+        targetView.layer.shadowRadius = 10
+    }
+    
+    @objc func edgePanAction(gesture: UIScreenEdgePanGestureRecognizer) {
+        guard let frontView = nextScreens.last?.view, 0 < nextScreens.count else { return }
+        let rootView = nextScreenTarget
+        let translation = gesture.translation(in: frontView)
+        let percentage = translation.x / frontView.frame.width
+        
+        if 0 < translation.x {
+            let rootViewX = slideWidthOnHide * (percentage - 1)
+            rootView.transform = CGAffineTransform(translationX: rootViewX, y: 0)
+            frontView.transform = CGAffineTransform(translationX: translation.x - rootViewX, y: 0)
+            
+            frontView.layer.shadowOpacity = Float(1.0 - percentage)
+            setMask(view: nextScreenTarget, x: -rootViewX)
         }
+        if rootView.transform != .identity && (gesture.state == .ended || gesture.state == .cancelled) {
+            let velocity = gesture.velocity(in: frontView)
+            if (frontView.frame.width / 2) < translation.x || 1000.0 < velocity.x {
+                let duration = TimeInterval(CGFloat(nextScreenAnimationDuration) * (1.0 - percentage))
+                removeNextScreenByGesture(parent: rootView, child: frontView, duration: duration)
+            } else {
+                let duration = TimeInterval(CGFloat(nextScreenAnimationDuration) * percentage)
+                forwardAnimation(parent: rootView, child: frontView, duration: duration)
+            }
+        }
+    }
+    
+    private func setShadowOpacityAnimation(view: UIView, fromValue: Float? = nil, toValue: Float, duration: TimeInterval, completion: (() -> Void)? = nil) {
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            completion?()
+        }
+        let animation = CABasicAnimation(keyPath: "shadowOpacity")
+        animation.fromValue = fromValue ?? view.layer.shadowOpacity
+        animation.duration = duration
+        animation.toValue = toValue
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        view.layer.add(animation, forKey: "shadowOpacity")
+        view.layer.shadowOpacity = toValue
+        
+        CATransaction.commit()
+    }
+    
+    private func setMask(view: UIView, x: CGFloat) {
+        let maskLayer = CAShapeLayer()
+        view.layer.mask = maskLayer
+        let rect = CGRect(x: x, y: 0, width: view.frame.width, height: view.frame.height)
+        let path = CGPath(rect: rect, transform: nil)
+        maskLayer.path = path
+    }
+    
+    // FIXME: マスク中に画面回転するとマスク範囲が不正になり見切れる
+    private func setMaskAnimation(view: UIView, x: CGFloat, duration: TimeInterval, completion: (() -> Void)? = nil) {
+        let originalMask = view.layer.mask as? CAShapeLayer
+        let maskLayer = originalMask ?? CAShapeLayer()
+        if originalMask == nil {
+            view.layer.mask = maskLayer
+        }
+        let rect = CGRect(x: x, y: 0, width: view.frame.width, height: view.frame.height)
+        let path = CGPath(rect: rect, transform: nil)
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            completion?()
+        }
+        let animation = CABasicAnimation(keyPath: "path")
+        animation.fromValue = maskLayer.path
+        animation.toValue = path
+        animation.duration = duration
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        maskLayer.add(animation, forKey: "path")
+        maskLayer.path = path
+        
+        CATransaction.commit()
+    }
+    
+    // MARK: - Support
+    
+    private func setMaskLayerRect(_ rect: CGRect, view: UIView) {
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = CGPath(rect: rect, transform: nil)
+        view.layer.mask = maskLayer
     }
     
     // MARK: - Overlay
@@ -308,5 +424,11 @@ open class SmartViewController: UIViewController {
                 $0.leave()
             }
         }
+    }
+}
+
+fileprivate extension CGRect {
+    func with(x: CGFloat) -> CGRect {
+        return CGRect(x: x, y: origin.y, width: size.width, height: size.height)
     }
 }
