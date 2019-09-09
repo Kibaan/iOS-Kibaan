db94e417e1fae0531d08e1c760718627e190bd4c
Keita Yamamoto <yamamoto@altonotes.co.jp>
2019-09-09 18:09:36 +0900

    URLSessionの開放処理を追加

--- Kibaan/Kibaan/HTTP/HTTPConnectorImpl.swift
+++ Kibaan/Kibaan/HTTP/HTTPConnectorImpl.swift
@@ -19,6 +19,9 @@ open class HTTPConnectorImpl: HTTPConnector {
         urlSessionTask = session.dataTask(with: request, completionHandler: { [weak self] (data, resp, err) in
             completionHandler(data, resp, err)
             self?.urlSessionTask = nil
+            // 一部のアプリでは以下の処理を入れないとメモリリークが発生する。
+            // 発生しないアプリもあり再現条件は不明だが明示的に破棄しておく
+            session.invalidateAndCancel()
         })
         urlSessionTask?.resume()
     }
